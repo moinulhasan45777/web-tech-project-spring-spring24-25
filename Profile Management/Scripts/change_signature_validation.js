06b2124f0f1d7b36ea40b1fb7f1766c19cc51a67
moinulhasan45777 <moinul.hasan45777@gmail.com>
2025-05-06 01:19:48 +0600

    Created DOCTOR PROFILE feature

---
+++ Profile Management/Scripts/change_signature_validation.js
@@ -0,0 +1,102 @@
+const form = document.getElementById("change-signature-form");
+const submitButton = document.getElementById("button-main");
+
+// ---------------------------------------------------------
+// ----------------Realtime Input Validation----------------
+// ---------------------------------------------------------
+
+// Digital Signature
+const clearButton = document.getElementById("clear-canvas");
+const canvas = document.getElementById("signature-canvas");
+const context = canvas.getContext("2d");
+const digitalSignatureInput = document.getElementById("digital-signature");
+
+function resizeCanvasToDisplaySize() {
+  const rect = canvas.getBoundingClientRect();
+  canvas.width = rect.width;
+  canvas.height = rect.height;
+}
+
+let drawing = false;
+let prevX = 0;
+let prevY = 0;
+
+resizeCanvasToDisplaySize();
+
+// Event listeners for drawing
+canvas.addEventListener("mousedown", (e) => {
+  canvas.style.outline = "2px solid #1c7340";
+  drawing = true;
+  prevX = e.clientX - canvas.getBoundingClientRect().left;
+  prevY = e.clientY - canvas.getBoundingClientRect().top;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (!drawing) return;
+  canvas.style.outline = "2px solid #1c7340";
+  draw(
+    e.clientX - canvas.getBoundingClientRect().left,
+    e.clientY - canvas.getBoundingClientRect().top
+  );
+});
+
+canvas.addEventListener("mouseup", () => {
+  drawing = false;
+});
+
+canvas.addEventListener("mouseleave", () => {
+  canvas.style.outline = "none";
+  drawing = false;
+});
+
+// Function to draw on the canvas
+function draw(x, y) {
+  context.beginPath();
+  context.strokeStyle = "#000";
+  context.lineWidth = 2;
+  context.lineJoin = "round";
+  context.moveTo(prevX, prevY);
+  context.lineTo(x, y);
+  context.closePath();
+  context.stroke();
+  prevX = x;
+  prevY = y;
+}
+
+function clearCanvas() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  digitalSignatureInput.value = "";
+}
+
+form.addEventListener("submit", function (e) {
+  digitalSignatureInput.value = canvas.toDataURL("image/png");
+});
+
+clearButton.addEventListener("click", clearCanvas);
+
+// ---------------------------------------------------------
+// ----------------On Submit Input Validation---------------
+// ---------------------------------------------------------
+
+// Function to check if the canvas is blank
+function isCanvasBlank(canvas) {
+  const context = canvas.getContext("2d");
+  const pixelBuffer = new Uint32Array(
+    context.getImageData(0, 0, canvas.width, canvas.height).data.buffer
+  );
+
+  return !pixelBuffer.some((color) => color !== 0);
+}
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (isCanvasBlank(canvas)) {
+    alert("Please provide a digital signature.");
+    canvas.style.outline = "2px solid #1c7340";
+    canvas.focus();
+    return;
+  }
+
+  form.submit();
+});
---
+++ Profile Management/Scripts/edit_profile_validation.js
@@ -0,0 +1,137 @@
+const form = document.getElementById("user-registration-form");
+const bloodGroup = document.getElementById("user-blood-group");
+const dob = document.getElementById("user-dob");
+const presentAddress = document.getElementById("user-address");
+const city = document.getElementById("user-city");
+const emName = document.getElementById("user-em-name");
+const emRelation = document.getElementById("user-em-relation");
+const emPhone = document.getElementById("user-em-phone");
+const submitButton = document.getElementById("button-main");
+const proPicture = document.getElementById("profile-picture");
+
+const changeAvatarButton = document.getElementById("change-avatar");
+const changeAvatarInput = document.getElementById("change-avatar-input");
+
+// ---------------------------------------------------------
+// ----------------Realtime Input Validation----------------
+// ---------------------------------------------------------
+
+// User Emergency Mobile-number Validation
+let Mobile = "";
+emPhone.addEventListener("input", function (e) {
+  let lastInputIndex = 15;
+
+  if (previousValueMobile == "") {
+    lastInputIndex = 0;
+  } else {
+    for (let i = 0; i < previousValueMobile.length - 1; i++) {
+      if (emPhone.value[i] != previousValueMobile[i]) {
+        lastInputIndex = i;
+        break;
+      }
+    }
+
+    if (lastInputIndex == 15) {
+      lastInputIndex = emPhone.value.length - 1;
+    }
+  }
+
+  if (
+    (emPhone.value[lastInputIndex] !== "+" &&
+      (emPhone.value[lastInputIndex] < "0" ||
+        emPhone.value[lastInputIndex] > "9")) ||
+    (emPhone.value[0] == "+" && emPhone.value.length > 14) ||
+    (emPhone.value[0] != "+" && emPhone.value.length > 11)
+  ) {
+    emPhone.value =
+      emPhone.value.slice(0, lastInputIndex) +
+      emPhone.value.slice(lastInputIndex + 1);
+  } else if (lastInputIndex != 0 && emPhone.value[lastInputIndex] == "+") {
+    emPhone.value =
+      emPhone.value.slice(0, lastInputIndex) +
+      emPhone.value.slice(lastInputIndex + 1);
+  } else if (
+    previousValueMobile != "" &&
+    emPhone.value[lastInputIndex] == "+" &&
+    emPhone.value[0] == "+" &&
+    emPhone.value.slice(1).includes("+")
+  ) {
+    emPhone.value = emPhone.value.slice(1);
+  }
+
+  previousValueMobile = emPhone.value;
+});
+
+// Date
+const dateInput = document.getElementById("user-dob");
+
+const today = new Date().toISOString().split("T")[0];
+dateInput.max = today;
+
+// Upload Profile Picture
+changeAvatarButton.addEventListener("click", function () {
+  changeAvatarInput.click();
+});
+
+changeAvatarInput.addEventListener("change", function () {
+  const file = changeAvatarInput.files[0];
+  const reader = new FileReader();
+  reader.onload = function (e) {
+    proPicture.src = e.target.result;
+  };
+  reader.readAsDataURL(file);
+});
+
+// ---------------------------------------------------------
+// ----------------On Submit Input Validation---------------
+// ---------------------------------------------------------
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (bloodGroup.value == "") {
+    alert("Please select a blood group.");
+    bloodGroup.focus();
+    return;
+  }
+
+  if (dob.value == "") {
+    alert("Please select a date of birth.");
+    dob.focus();
+    return;
+  }
+
+  // Formatting Date of Birth
+  const dobDate = dob.value.split("-");
+
+  if (presentAddress.value.length < 5) {
+    alert("Please enter a valid address.");
+    presentAddress.focus();
+    return;
+  }
+
+  if (city.value.length < 3) {
+    alert("Please enter a valid city name.");
+    city.focus();
+    return;
+  }
+
+  if (emName.value.length < 3) {
+    alert("Please enter a valid emergency contact name.");
+    emName.focus();
+    return;
+  }
+
+  if (emRelation.value.length < 3) {
+    alert("Please enter a valid emergency contact relation.");
+    emRelation.focus();
+    return;
+  }
+
+  if (emPhone.value.length != 11 && emPhone.value.length != 14) {
+    alert("Please enter a valid emergency contact number.");
+    emPhone.focus();
+    return;
+  }
+  form.submit();
+});
